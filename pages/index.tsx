54873f7133ec955e8c8caca540293838f5a980be
JOAQUIN SANDOVAL MIRAMONTES <joakas.11@gmail.com>
2023-08-27 14:18:21 -0600

    Feat: Deleted template style and added custom style.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import localFont from "next/font/local";
+import {VT323} from "next/font/google"
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
-const cagatonDate = new Date('2023-09-05');
+const cagatonDate = new Date("2023-09-05");
+const vt323Font = VT323({weight: '400',subsets: ['latin']});
+
 
 export default function Home() {
+  const [currDate, setCurrDate] = useState(
+    cagatonDate.getTime() - new Date().getTime()
+  );
 
-
-  const [currDate, setCurrDate] = useState(cagatonDate.getTime() - new Date().getTime());
-
-  setInterval(function() {
+  setInterval(function () {
     setCurrDate(cagatonDate.getTime() - new Date().getTime());
-  }, 1000)
+  }, 1000);
 
   return (
     <>
@@ -25,27 +27,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <main className={styles.main}>
+      <main className={`${styles.main} ${vt323Font.className}`}>
         <h1>Faltan</h1>
-        <h2 className={styles.timer}>{getRemainingTime(currDate)}</h2>
+        <h2 className={styles.timer} suppressHydrationWarning={true}>
+          {getRemainingTime(currDate)}
+        </h2>
         <h3>... para el cagatón</h3>
       </main>
     </>
-  )
+  );
 }
 
-
-function getDaysFromMillisecons(time : number) {
+function getDaysFromMillisecons(time: number) {
   return Math.floor(time / (1000 * 60 * 60 * 24));
 }
 
-function getHoursFromMilliseconds(time : number) {
+function getHoursFromMilliseconds(time: number) {
   return Math.floor(time / (1000 * 60 * 60));
 }
 
-function getMinutesFromMilliseconds(time : number) {
+function getMinutesFromMilliseconds(time: number) {
   return Math.floor(time / (1000 * 60));
 }
 
@@ -53,19 +59,18 @@ function getSecondsFromMilliseconds(time: number) {
   return Math.floor(time / 1000);
 }
 
-
 function getRemainingTime(time: number) {
   let tempTime = time;
-  const days = String(getDaysFromMillisecons(tempTime)).padStart(2,'0');
+  const days = String(getDaysFromMillisecons(tempTime)).padStart(2, "0");
   tempTime = tempTime % (1000 * 60 * 60 * 24);
 
-  const hours = String(getHoursFromMilliseconds(tempTime)).padStart(2,'0');
+  const hours = String(getHoursFromMilliseconds(tempTime)).padStart(2, "0");
   tempTime = tempTime % (1000 * 60 * 60);
 
-  const minutes = String(getMinutesFromMilliseconds(tempTime)).padStart(2,'0');
-  tempTime = tempTime % (1000 * 60)
+  const minutes = String(getMinutesFromMilliseconds(tempTime)).padStart(2, "0");
+  tempTime = tempTime % (1000 * 60);
 
-  const seconds = String(getSecondsFromMilliseconds(tempTime)).padStart(2,'0');
+  const seconds = String(getSecondsFromMilliseconds(tempTime)).padStart(2, "0");
 
-  return `${days}:${hours}:${minutes}:${seconds}`;
+  return `${days} días ${hours} horas ${minutes} minutos ${seconds} segundos`;
 }
